baa79dcd5851566fbad34f08371d4c5c06efd841
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-05-31 16:09:01 +0900

    Update Hero.java

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -2,11 +2,4 @@ public class Hero
 {
     String name;
     int hp;
-    
-    public void attack(Matango mt)
-    {
-        System.out.println(
-            "勇者" + this.name + "はお化けキノコ" + mt.suffix + "を攻撃した!");
-        mt.hp -= 20;
-    }
 }

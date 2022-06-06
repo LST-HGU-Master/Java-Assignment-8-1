f9ae52008f4eb89bb0ba8e5669fa86cb95378506
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-06-06 15:32:01 +0900

    Update HeroTest.java

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -2,7 +2,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import java.io.*;
 /**
- * @version (20220527)
+ * @version (20220606)
  * 
  * (注意) Heroクラス内に attack(Matango m) が
  * 　　　　宣言されるまで、このテストクラスはエラーが表示される
@@ -12,9 +12,9 @@ public class HeroTest {
     @Test
     public void testDamage20()
     {
-        PrintStream originalOut = System.out;
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(bos));
+        // PrintStream originalOut = System.out;
+        // ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        // System.setOut(new PrintStream(bos));
 
         // action
         Hero h = new Hero();
@@ -25,17 +25,10 @@ public class HeroTest {
         m.suffix = 'X';
 
         h.attack(m);
-
-        // assertion
-        try {
-            assertEquals(130, m.hp,
-                "Hero.attack(Matango )後のmatangoインスタンスのhpが不正です!");
-        } catch (AssertionError err) {
-            System.setOut(originalOut);
-            throw err;
-        }
         // undo the binding in System
-        System.setOut(originalOut);
+        // System.setOut(originalOut);
+        // assertion
+        assertEquals(130, m.hp, "Hero.attack(Matango )中のhpの処理が不正です!");
     }
 
     @Test
@@ -54,16 +47,12 @@ public class HeroTest {
         m.suffix = 'X';
 
         h.attack(m);
-
-        // assertion
-        String[] prints = bos.toString().split("\r\n|\n");
-        try {
-            assertEquals("勇者コータローはお化けキノコXを攻撃した!", prints[0],"Hero.attack(Matango )のprint出力が実行例と異なります!");
-        } catch (AssertionError err) {
-            System.setOut(originalOut);
-            throw err;
-        }
          // undo the binding in System
         System.setOut(originalOut);
+        // assertion
+        String[] prints = bos.toString().split("\r\n|\n", -1);
+        assertEquals("勇者コータローはお化けキノコXを攻撃した!", prints[0],"Hero.attack(Matango )のprint出力が実行例と異なります!");
+        assertEquals(2, prints.length, "Hero.attack(Matango )のprint出力で改行が２つ以上あります!");
+
     }
 }

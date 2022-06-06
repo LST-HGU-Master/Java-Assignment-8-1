91f7c933cf95be0cdf7f715cb23ca1b26f1b7b50
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-06-06 15:33:34 +0900

    Update Prog81Test.java

--- src/test/java/Prog81Test.java
+++ src/test/java/Prog81Test.java
@@ -3,7 +3,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
 import java.io.*;
 /**
- * @version (20220527)
+ * @version (20220606)
  **/
 public class Prog81Test {
 
@@ -23,23 +23,18 @@ public class Prog81Test {
             bos = new ByteArrayOutputStream();
             System.setOut(new PrintStream(bos));
             Prog81.main(new String[]{String.valueOf(hp)});
-
+            // undo the binding in System
+            System.setOut(originalOut);
             // assertion
-            String[] prints = bos.toString().split("\r\n|\n");
+            String[] prints = bos.toString().split("\r\n|\n", -1);
             try {
-                assertEquals("勇者のHP: " + (hp-20), prints[prints.length - 3],"main()での処理による勇者のHPの変化が予定通りでありません!");
-                assertEquals("お化けキノコAのHP: " + (hp-20), prints[prints.length - 2],"main()での処理によるおばけキノコAのHPの変化が予定通りでありません!");
-                assertEquals("お化けキノコBのHP: " + (hp-20), prints[prints.length - 1],"main()での処理によるおばけキノコBのHPの変化が予定通りでありません!");
-            } catch (AssertionError err) {
-                System.setOut(originalOut);
-                throw err;
+                assertEquals("勇者のHP: " + (hp-20), prints[prints.length - 4],"main()での処理による勇者のHPの変化が予定通りでありません!");
+                assertEquals("お化けキノコAのHP: " + (hp-20), prints[prints.length - 3],"main()での処理によるおばけキノコAのHPの変化が予定通りでありません!");
+                assertEquals("お化けキノコBのHP: " + (hp-20), prints[prints.length - 2],"main()での処理によるおばけキノコBのHPの変化が予定通りでありません!");
             } catch (ArrayIndexOutOfBoundsException excpt) {
-                System.setOut(originalOut);
                 fail("Prog81.main()のprint出力に改行が３つ分ありません!");
             }
+            assertEquals(7, prints.length,"print命令の改行が７つ以上あります!");
         }
-
-        // undo the binding in System
-        System.setOut(originalOut);
     }
 }
